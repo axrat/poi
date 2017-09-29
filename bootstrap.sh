0630a32a114d20a44dcef25a70bef7687ba083a6
onoie <onoie3@gmail.com>
2017-09-30 08:35:53 +0900

    add main & bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ "$(id -u)" != "0" ]]; then
+    PASS=$(zenity --entry --text="input:")
+    CMD=$0
+    expect -c "
+      set timeout -1
+      spawn sudo $CMD
+      expect \"assword\" {
+        send \"$PASS\n\"
+      }
+      interact
+    "
+    exit
+fi
+sudo echo "bootstrap"
+
+echo "complete"
+
